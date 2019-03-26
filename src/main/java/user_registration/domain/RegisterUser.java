9a6371e290be9eb3aff141d7af8c4d3db9d019dd
Luis Rovirosa <luisrovirosa@gmail.com>
2019-03-26 16:26:36 +0100

    creates RegisterUser but it's still coupled with infrastructure

---
+++ src/main/java/user_registration/domain/RegisterUser.java
@@ -0,0 +1,56 @@
+package user_registration.domain;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import user_registration.infrastructure.UserRegistrationController;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Properties;
+import java.util.Random;
+
+public class RegisterUser {
+    public ResponseEntity execute(HttpServletRequest request) throws MessagingException {
+        if (request.getParameter("password").length() <= 8 || !request.getParameter("password").contains("_")) {
+            return new ResponseEntity("The password is not valid", HttpStatus.BAD_REQUEST);
+        }
+
+        if (UserRegistrationController.orm.findByEmail(request.getParameter("email")) != null) {
+            return new ResponseEntity("The email is already in use", HttpStatus.BAD_REQUEST);
+        }
+
+        User user = new User(
+                new Random().nextInt(),
+                request.getParameter("name"),
+                request.getParameter("email"),
+                request.getParameter("password")
+        );
+        UserRegistrationController.orm.save(user);
+
+        Properties prop = new Properties();
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("smtpUsername", "smtpPassword");
+            }
+        });
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("noreply@codium.team"));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(request.getParameter("email")));
+        message.setSubject("Welcome to Codium");
+        String msg = "This is the confirmation email";
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(msg, "text/html");
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+        message.setContent(multipart);
+        // If a proper SMTP server is configured, this line could be uncommented
+        // Transport.send(message);
+
+        return ResponseEntity.ok(user);
+    }
+}
